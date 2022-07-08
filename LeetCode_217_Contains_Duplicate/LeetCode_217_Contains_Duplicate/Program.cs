ac209fdfd55709b76b34698984cdb97e7c809c73
aurpan-dash <aurpan.dash@gmail.com>
2022-07-08 23:58:46 +0600

    Problem - 217

---
+++ LeetCode_217_Contains_Duplicate/LeetCode_217_Contains_Duplicate/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode_217_Contains_Duplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+
+            bool returnVal = new Solution().ContainsDuplicate(nums);
+
+            Console.WriteLine(returnVal);
+
+        }
+    }
+
+}
---
+++ LeetCode_217_Contains_Duplicate/LeetCode_217_Contains_Duplicate/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_217_Contains_Duplicate
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> hashNums = new HashSet<int>();
+            hashNums.Add(nums[0]);
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (hashNums.Contains(nums[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    hashNums.Add(nums[i]);
+                }
+            }
+            return false;
+        }
+    }
+}
