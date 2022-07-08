56fc7172855b963d090a67fd0bead0e38edecd39
aurpan-dash <aurpan.dash@gmail.com>
2022-07-09 02:27:25 +0600

    Problem - 53 and 1

---
+++ LeetCode_1_Two_Sum/LeetCode_1_Two_Sum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_1_Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int target = 4;
+            int[] nums = { 3, 3, 3, 3, 1 };
+
+            nums = new Solution().TwoSum(nums, target);
+            Console.WriteLine("{0},{1}",nums[0], nums[1]);
+        }
+    }
+
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            dictionary.Add(nums[0], 0);
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (dictionary.ContainsKey(target - nums[i]))
+                {
+                    result[0] = i;
+                    result[1] = dictionary[target - nums[i]];
+                    break;
+                }
+                else if (!dictionary.ContainsKey(nums[i]))
+                {
+                    dictionary.Add(nums[i], i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode_53_Maximum_Subarray/LeetCode_53_Maximum_Subarray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_53_Maximum_Subarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -3, -1, -8 };
+            int maxSum = new Solution().MaxSubArray(nums);
+
+            Console.WriteLine(maxSum);
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int sum = nums[0], maxVal = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                sum += nums[i];
+
+                if (sum < nums[i])
+                {
+                    sum = nums[i];
+                }
+                if (sum > maxVal)
+                {
+                    maxVal = sum;
+                }
+            }
+
+            return maxVal;
+        }
+    }
+}
