e1a4fa8a2ee25e09f555e1eab091c0ed77416119
aurpan-dash <aurpan.dash@gmail.com>
2022-07-11 01:10:15 +0600

    prob 350 solved

--- LeetCode_350._Intersection_of_Two_Arrays_II/LeetCode_350._Intersection_of_Two_Arrays_II/Program.cs
+++ LeetCode_350._Intersection_of_Two_Arrays_II/LeetCode_350._Intersection_of_Two_Arrays_II/Program.cs
@@ -10,8 +10,8 @@ namespace LeetCode_350._Intersection_of_Two_Arrays_II
     {
         static void Main(string[] args)
         {
-            int[] nums1 = { };
-            int[] nums2 = { };
+            int[] nums1 = { 4, 9, 5 };
+            int[] nums2 = { 9, 4, 9, 8, 4 };
 
             int[] returnVal = new Solution().Intersect(nums1, nums2);
             Console.Write("");
@@ -22,8 +22,31 @@ namespace LeetCode_350._Intersection_of_Two_Arrays_II
     {
         public int[] Intersect(int[] nums1, int[] nums2)
         {
-            int[] retVal = new int[2];
-            return retVal;
+            List<int> liValues = new List<int>();
+            Array.Sort(nums1);
+            Array.Sort(nums2);
+            int m = nums1.Length - 1;
+            int n = nums2.Length - 1;
+
+            while (m >= 0 && n >=0)
+            {
+                if (nums1[m] == nums2[n])
+                {
+                    liValues.Add(nums1[m]);
+                    m--;
+                    n--;
+                }
+                else if (nums1[m] < nums2[n])
+                {
+                    n--;
+                }
+                else if (nums1[m] > nums2[n])
+                {
+                    m--;
+                }
+            }
+
+            return liValues.ToArray();
         }
     }
 }
