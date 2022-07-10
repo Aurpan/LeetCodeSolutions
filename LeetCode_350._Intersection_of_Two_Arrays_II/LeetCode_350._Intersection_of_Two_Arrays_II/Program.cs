70b6a3ac96ce18f9bbe27e04f57969011f2ffb8c
aurpan-dash <aurpan.dash@gmail.com>
2022-07-11 00:33:22 +0600

    Problem - 350

---
+++ LeetCode_350._Intersection_of_Two_Arrays_II/LeetCode_350._Intersection_of_Two_Arrays_II/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_350._Intersection_of_Two_Arrays_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { };
+            int[] nums2 = { };
+
+            int[] returnVal = new Solution().Intersect(nums1, nums2);
+            Console.Write("");
+        }
+    }
+
+    public class Solution
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            int[] retVal = new int[2];
+            return retVal;
+        }
+    }
+}
