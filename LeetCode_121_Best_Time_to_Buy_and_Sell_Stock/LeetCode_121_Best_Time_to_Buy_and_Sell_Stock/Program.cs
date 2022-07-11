7eac1ade526434b7480e923dca87f4e54ae8e3d7
aurpan-dash <aurpan.dash@gmail.com>
2022-07-11 20:14:24 +0600

    Prob - 121

---
+++ LeetCode_121_Best_Time_to_Buy_and_Sell_Stock/LeetCode_121_Best_Time_to_Buy_and_Sell_Stock/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_121_Best_Time_to_Buy_and_Sell_Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = { 2,1,2,1,0,1,2 };
+
+            int profit = new Solution().MaxProfit(prices);
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            int buyIndx = 0, sellIndx = 1;
+
+            while (sellIndx < prices.Length)
+            {
+                if ((prices[sellIndx] - prices[buyIndx]) < 0)
+                {
+                    buyIndx = sellIndx;
+                    sellIndx++;
+                }
+                else
+                {
+                    maxProfit = Math.Max(prices[sellIndx] - prices[buyIndx], maxProfit);
+                    sellIndx++;
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
