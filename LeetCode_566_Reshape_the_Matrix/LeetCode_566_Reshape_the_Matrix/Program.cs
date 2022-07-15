d8208c0f64d45c1923c2ffad9ab500a68c72fa22
aurpan-dash <aurpan.dash@gmail.com>
2022-07-15 23:53:30 +0600

    Problem 566 not solved

---
+++ LeetCode_566_Reshape_the_Matrix/LeetCode_566_Reshape_the_Matrix/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_566_Reshape_the_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
