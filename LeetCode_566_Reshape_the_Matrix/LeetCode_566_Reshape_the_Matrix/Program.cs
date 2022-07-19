f61eb52c6c8c227f800b2390e437b668d7d7d10e
aurpan-dash <aurpan.dash@gmail.com>
2022-07-19 23:46:03 +0600

    Problem 118 not solved

--- LeetCode_566_Reshape_the_Matrix/LeetCode_566_Reshape_the_Matrix/Program.cs
+++ LeetCode_566_Reshape_the_Matrix/LeetCode_566_Reshape_the_Matrix/Program.cs
@@ -10,6 +10,42 @@ namespace LeetCode_566_Reshape_the_Matrix
     {
         static void Main(string[] args)
         {
+            //int r = 0, c = 0;
+            //int[2][] mat = { { 1, 2 }, { 3, 4 } };
+
+            //var x = new Solution().MatrixReshape(mat, r, c);
+            //Console.Write("");
+        }
+    }
+
+    public class Solution
+    {
+        public int[][] MatrixReshape(int[][] mat, int r, int c)
+        {
+            int m = mat.Length;
+            int n = mat[0].Length;
+
+            if (m * n != r * c)
+            {
+                return mat;
+            }
+
+            int[][] result = new int[r][];
+            int index, a, b;
+            for (int i = 0; i < r; i++)
+            {
+                int[] mid = new int[c];
+                for (int j = 0; j < c; j++)
+                {
+                    index = i * c + j;
+                    a = index / n;
+                    b = index - a * n;
+                    mid[j] = mat[a][b];
+                }
+                result[i] = mid;
+            }
+
+            return result;
         }
     }
 }
