85a10de6b0f31714610f6bca5e7f8e7c80cb1089
aurpan-dash <aurpan.dash@gmail.com>
2022-07-20 01:45:39 +0600

    118 not solved yet

--- LeetCode_118_Pascal's_Triangle/LeetCode_118_Pascal's_Triangle/Program.cs
+++ LeetCode_118_Pascal's_Triangle/LeetCode_118_Pascal's_Triangle/Program.cs
@@ -10,6 +10,24 @@ namespace LeetCode_118_Pascal_s_Triangle
     {
         static void Main(string[] args)
         {
+            int numRows = 5;
+
+            var result = new Solution().Generate(numRows);
+        }
+
+        public class Solution
+        {
+            public IList<IList<int>> Generate(int numRows)
+            {
+                IList<IList<int>> retVal = null;
+
+
+                if (numRows < 2)
+                    return [[1]];
+
+
+                return retVal; 
+            }
         }
     }
 }
